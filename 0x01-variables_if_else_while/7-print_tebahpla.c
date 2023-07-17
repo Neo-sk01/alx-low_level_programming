2e8af975b22667d012fc146da8441e2e28a22e09
Neo Sekaleli <neosekaleli@gmail.com>
2023-07-17 21:37:38 +0200

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main - Entry point 
+*main - Entry point
 *Description: 'print alphabets in reverse'
 *Return: Always 0
 */
 int main(void)
 {
 int n = 122;
-while( n >= 97)
+while (n >= 97)
 {
 putchar(n);
 n--;
