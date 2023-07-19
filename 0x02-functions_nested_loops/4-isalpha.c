11059d75265c98b5c39aab1b50799cc06a76a5e2
Neo Sekaleli <neosekaleli@gmail.com>
2023-07-19 17:46:49 +0200

    4-main.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_isalpha - checks for alphabetic character
+  *@c: the character to be checkede
+  *Return: just another day at ALX SE 
+  *
+  */
+
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
